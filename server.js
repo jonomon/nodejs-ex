93376774d586186ef07a8c3336253338777879d5
Jonathan Chung <jono.chung@gmail.com>
2016-07-21 14:52:09 -0400

    Added signup

--- server.js
+++ server.js
@@ -76,18 +76,20 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/pagecount', function (req, res) {
+app.get('/signup', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
     initDb(function(err){});
   }
   if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
+      var username = req.query.username;
+      var secretQuestion = req.query.sq;
+      var secretAnswer = req.query.sq;
+      res.send('{ username: ' + username + ', sq: ' + secretQuestion + ', sa: ' + secretAnswer+ '}');
+    //db.collection('counts').count(function(err, count ){
+    //  res.send('{ pageCount: ' + count + '}');
+    //});
   }
 });
 
