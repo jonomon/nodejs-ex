4db587753ccf6d59b8aaede9215c34d50321f1d3
jonomon <jono.chung@gmail.com>
2016-07-21 14:07:14 -0400

    Added login and signup skeloton

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var express = require('express'),
     eps     = require('ejs'),
     morgan  = require('morgan');
     
-Object.assign=require('object-assign')
+Object.assign = require('object-assign');
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -53,42 +53,64 @@ var initDb = function(callback) {
     dbDetails.databaseName = db.databaseName;
     dbDetails.url = mongoURLLabel;
     dbDetails.type = 'MongoDB';
-
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
 
 app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
+    if (!db)
+    {
+	initDb(function(err){});
+    }
+    if (db)
+    {
+	var col = db.collection('counts');
+	col.insert({ip: req.ip, date: Date.now()});
+	col.count(function(err, count){
+	    res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+	});
+    } else {
+	res.render('index.html', { pageCountMessage : null});
   }
 });
 
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
+    if (!db)
+    {
+	initDb(function(err){});
+    }
+    if (db)
+    {
+	db.collection('counts').count(function(err, count ){
+	    res.send('{ pageCount: ' + count + '}');
+	});
+    }
+    else
+    {
+	res.send('{ pageCount: -1 }');
+    }
+});
+
+app.get('/login', function(req, res) {
+    if (!db) {
+	initDb(function(err){});
+    }
+    if (db)
+    {
+	res.send('{ success: true}');
+    }
+});
+
+app.get('/signup', function(req, res) {
+    if (!db) {
+	initDb(function(err){});
+    }
+    if (db)
+    {
+	res.send('{ success: true}');
+    }
 });
 
 // error handling
